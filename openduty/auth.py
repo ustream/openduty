47179a976d1602e1748ed07e066401da66d520d8
deathowl <csergo.balint@ustream.tv>
2014-12-29 17:39:02 +0100

    fix  for issue #21. Redirection works correctly for pages that require a login

--- openduty/auth.py
+++ openduty/auth.py
@@ -3,17 +3,18 @@ from django.template.response import TemplateResponse
 from django.contrib.auth import authenticate, login as auth_login, logout as auth_logout;
 
 def login(request):
-    return TemplateResponse(request, 'auth/login.html', {})
+    redirect_to = request.GET.get("next", "/dashboard/")
+    return TemplateResponse(request, 'auth/login.html', {"redirect_to": redirect_to})
 
 def do(request):
     username = request.POST['username']
     password = request.POST['password']
     user = authenticate(username=username, password=password)
-
+    redirect_to = request.POST['redirect_to']
     if user is not None:
         if user.is_active:
             auth_login(request, user)
-            return HttpResponseRedirect('/dashboard/')
+            return HttpResponseRedirect(redirect_to)
     else:
         return HttpResponseRedirect('/login/')
 
