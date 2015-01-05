3ca040c171c27d0d927f47e3844f51b8321f1701
deathowl <csergo.balint@ustream.tv>
2015-01-05 18:25:55 +0100

    remove timeout.

--- openduty/healthcheck.py
+++ openduty/healthcheck.py
@@ -23,18 +23,18 @@ class HealthCheckViewSet(viewsets.ModelViewSet):
             return Response("FAILED", status=status.HTTP_500_INTERNAL_SERVER_ERROR)
         return Response("OK", status=status.HTTP_200_OK)
 
+
 class CeleryHealthCheckViewSet(viewsets.ModelViewSet):
     queryset = Incident.objects.all()
     serializer_class = NoneSerializer
-
+    
     def list(self, request):
         try:
             timestamp = int(time())
             random = randint(0, 100000)
-            result = add.apply_async(args=[timestamp, random],
-                                     expires=datetime.datetime.now() + datetime.timedelta(seconds=3), connect_timeout=3)
+            result = add.apply_async(args=[timestamp, random])
             now = datetime.datetime.now()
-            while (now + datetime.timedelta(seconds=5)) > datetime.datetime.now():
+            while (now + datetime.timedelta(seconds=10)) > datetime.datetime.now():
                 if result.result == timestamp + random:
                     return Response("OK", status=status.HTTP_200_OK)
                 sleep(0.5)
