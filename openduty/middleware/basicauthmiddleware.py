deca7030f8bdcf8d8c70544e717de443f5dc152c
deathowl <csergo.balint@ustream.tv>
2015-01-30 16:39:53 +0100

    basicauth middleware working

--- openduty/middleware/basicauthmiddleware.py
+++ openduty/middleware/basicauthmiddleware.py
@@ -1,13 +1,17 @@
-from django.http import HttpResponse
+from django.contrib.auth import authenticate, login as auth_login, logout as auth_logout;
+from django.http import HttpResponse, HttpResponseRedirect
 from django.conf import settings
 
 class BasicAuthMiddleware(object):
 
-
     def process_request(self,request):
-        authentication = request.META['HTTP_AUTHORIZATION']
-        (authmeth, auth) = authentication.split(' ',1)
-        if 'basic' != authmeth.lower():
-            return self.unauthed()
-        auth = auth.strip().decode('base64')
-        username, password = auth.split(':',1)
+        authentication = request.META.get('HTTP_AUTHORIZATION')
+        if authentication:
+            (authmeth, auth) = authentication.split(' ',1)
+            if 'basic' == authmeth.lower():
+                auth = auth.strip().decode('base64')
+                username, password = auth.split(':',1)
+                user = authenticate(username=username, password=password)
+                if user is not None:
+                    if user.is_active:
+                        auth_login(request, user)
