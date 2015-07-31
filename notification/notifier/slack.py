6dd658c4f4c910a5dfa8ecb2d82858a4092d893f
deathowl <bagoly@endticket.com>
2015-07-31 17:40:34 +0200

    add fancy icon and name to slack integration for fun and profit.

--- notification/notifier/slack.py
+++ notification/notifier/slack.py
@@ -7,7 +7,8 @@ class SlackNotifier:
     
     def notify(self, notification):
         slack = Slacker(self.__config['apikey'])
-        response = slack.chat.post_message(notification.user_to_notify.profile.slack_room_name, notification.message)
+        response = slack.chat.post_message(notification.user_to_notify.profile.slack_room_name, notification.message,
+                                           username="Openduty", icon_url="https://slack.global.ssl.fastly.net/1937/img/services/pagerduty_48.png")
         if not response.error:
             print "Slack message sent"
         else:
